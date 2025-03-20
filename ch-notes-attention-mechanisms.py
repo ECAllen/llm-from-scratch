e4f936549a5a82c2b593c269d792698bf7080073
ECAllen <1305173+ECAllen@users.noreply.github.com>
2025-03-19 23:16:44 -0400

    updates to attention

--- ch-notes-attention-mechanisms.py
+++ ch-notes-attention-mechanisms.py
@@ -1,6 +1,7 @@
 """ Ch 3 - Attention Mechanisms """
 
 import torch
+import torch.nn as nn
 
 inputs = torch.tensor(
     [
@@ -32,3 +33,51 @@ def softmax_naive(x):
 attn_weights_2_naive = softmax_naive(attn_scores_2)
 print("Attention weights (naive):", attn_weights_2_naive)
 print("Sum  (naive): ", attn_weights_2_naive.sum())
+
+x_2 = inputs[1]  # 1
+d_in = inputs.shape[1]  # 2
+d_out = 2
+
+
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        attn_scores = queries @ keys.T  # omega
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+print(sa_v1(inputs))
+
+
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
