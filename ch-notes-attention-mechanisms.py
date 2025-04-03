9dea64c67f8f796dcfa755492f8a6bfdcc08aaf3
ECAllen <1305173+ECAllen@users.noreply.github.com>
2025-04-03 00:36:27 -0400

    more notes

--- ch-notes-attention-mechanisms.py
+++ ch-notes-attention-mechanisms.py
@@ -81,3 +81,21 @@ class SelfAttention_v2(nn.Module):
 torch.manual_seed(789)
 sa_v2 = SelfAttention_v2(d_in, d_out)
 print(sa_v2(inputs))
+
+queries = sa_v2.W_query(inputs)
+keys = sa_v2.W_key(inputs)
+attn_scores = queries @ keys.T
+attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+print(attn_weights)
+
+context_length = attn_scores.shape[0]
+print(f"context_length: {context_length}")
+mask_simple = torch.tril(torch.ones(context_length, context_length))
+print(mask_simple)
+
+masked_simple = attn_weights * mask_simple
+print(f"masked_simple: {masked_simple}")
+
+row_sums = masked_simple.sum(dim=-1, keepdim=True)
+masked_simple_norm = masked_simple / row_sums
+print(f"masked_simple_norm: {masked_simple_norm}")
